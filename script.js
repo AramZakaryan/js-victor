961a37c0433a411186a11f11fe71dec5887d443c
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-07-13 23:49:23 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Theme 02. Array Mathods 
+
+const student = [
+    {
+        name: 'Bob',
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: 'Alex',
+        age: 21,
+        isMarried: true,
+        scores: 89,
+    },
+    {
+        name: 'Nick',
+        age: 20,
+        isMarried: false,
+        scores: 85,
+    },
+    {
+        name: 'John',
+        age: 19,
+        isMarried: false,
+        scores: 100,
+    },
+]
