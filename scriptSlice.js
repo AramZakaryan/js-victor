39cee536ad1fe78270acd1155f05f0257c85b7ea
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-07-14 17:04:04 +0400

    Interim Result of Lesson 02 (Array Methods) (Result of Part regarding method Slice)

---
+++ scriptSlice.js
@@ -0,0 +1,39 @@
+// Theme 02. Array Methods (Part regarding method FILTER) 
+
+const student = [
+    {
+        name: 'Bob',
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: 'Alex',
+        age: 21,
+        isMarried: true,
+        scores: 89,
+    },
+    {
+        name: 'Nick',
+        age: 20,
+        isMarried: false,
+        scores: 85,
+    },
+    {
+        name: 'John',
+        age: 19,
+        isMarried: false,
+        scores: 100,
+    },
+]
+
+
+const mySlice = (arr, indStart, indEnd) => {
+    let res = []
+    for (let i = indStart; i < indEnd; i++) {
+        res.push(arr[i])
+    }
+    return res
+}
+
+console.log(mySlice(student, 1, 3))
